dcb30f0a044a08479557f5d77702664c5ae925f8
Sebastian Döll <sebastian@katallaxie.dev>
2026-07-26 21:15:29 +0200

    wip: add files controller

---
+++ server/adapters/files/files.go
@@ -0,0 +1,37 @@
+package files
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"github.com/katallaxie/pkg/filex"
+	"github.com/zeiss/builder/internal/models"
+	"github.com/zeiss/builder/server/configs"
+	"github.com/zeiss/builder/server/ports"
+)
+
+var _ ports.Files = (*Files)(nil)
+
+// Files is the database adapter for the builder server.
+type Files struct {
+	cfg *configs.Config
+}
+
+// NewFiles creates a new Files instance.
+func NewFiles(cfg *configs.Config) *Files {
+	return &Files{cfg: cfg}
+}
+
+func (f *Files) UploadFile(ctx context.Context, site *models.Site, file *models.File) error {
+	path := filepath.Join(f.cfg.Flags.FilesFlags.Path, site.Name, file.Name)
+	if err := filex.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(path, file.Data, 0o644); err != nil {
+		return err
+	}
+
+	return nil
+}
